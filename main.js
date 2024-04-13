0b60d14d6ca8b73b45a289a83fd99c06114d0426
HSU, Li-hsiu <lhsuaa@connect.ust.hk>
2024-04-13 20:50:50 +0800

    dragControl

--- main.js
+++ main.js
@@ -1,86 +1,44 @@
-import * as THREE from 'three'
+import * as THREE from 'three';
 import { createBox, createFloor } from './objects.js';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
-import { DragControls } from 'three/addons/controls/DragControls.js';
+import { createLight } from './lights.js';
+import { initControl } from './controls.js';
+import { VRButton } from 'three/addons/webxr/VRButton.js';
 
-var scene, camera, renderer, ambientLight, directionalLight, objects, oControls, dControls, plane, intersects, raycaster, touch;
+var scene, camera, renderer, objects;
 
 function init() {
-  // camera
-  camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 1500);
-  camera.position.set(-35, 70, 100);
-  camera.lookAt(new THREE.Vector3(0, 0, 0));
+  const stageCenter = new THREE.Vector3(0,0,70);
+  const stageScale = new THREE.Vector3(100,2,70);
 
-  // renderer
+  camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 1500);
+  camera.position.set(70,70,-70);
+
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.shadowMap.enabled = true;
   document.body.appendChild(renderer.domElement);
 
-  // scene
   scene = new THREE.Scene()
-  scene.background = new THREE.Color(0xbfd1e5);
+  scene.background = new THREE.Color(0x1a1a1a);
 
-  // ambient light
-  ambientLight = new THREE.AmbientLight(0xffffff, 0.20);
-  scene.add(ambientLight);
-
-  // directional light
-  directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-  directionalLight.position.set(-30, 50, -30);
-  scene.add(directionalLight);
-  directionalLight.castShadow = true;
-  directionalLight.shadow.mapSize.width = 2048;
-  directionalLight.shadow.mapSize.height = 2048;
-  directionalLight.shadow.camera.left = -70;
-  directionalLight.shadow.camera.right = 70;
-  directionalLight.shadow.camera.top = 70;
-  directionalLight.shadow.camera.bottom = -70;
+  createLight(scene);
 
   objects = [];
 
-  createFloor(scene);
-  createBox(scene, objects);
+  var floor = createFloor(scene, stageCenter, stageScale);
+  var box = createBox(scene, objects);
 
-  // resize
+  var seat = new THREE.Mesh(new THREE.BoxBufferGeometry(), new THREE.MeshPhongMaterial({ color: 0xFFFFFF }));
+  seat.position.set(0,0,0);
+  scene.add(seat);
+
+  initControl(objects, camera, renderer, stageCenter, stageScale);
+
   window.addEventListener('resize', onWindowResize);
-  //window.addEventListener( 'mousemove', onMouseMove, false );
-  window.addEventListener('touchmove', onTouchMove, false);
 
-  plane = new THREE.Plane(new THREE.Vector3(0, 1, 0), 0);
-  raycaster = new THREE.Raycaster();
-  intersects = new THREE.Vector3();
-
-  // orbit control
-  oControls = new OrbitControls(camera, renderer.domElement);
-  oControls.maxDistance = 300;
-  oControls.minDistance = 50;
-  oControls.maxPolarAngle = Math.PI/2;
-
-  touch = new THREE.Vector2();
-
-  function onTouchMove(e) { 
-    e.preventDefault(); 
-    touch.x = (e.touches[0].clientX / window.innerWidth) * 2 - 1; 
-    touch.y = -(e.touches[0].clientY / window.innerHeight) * 2 + 1; 
-  }
-
-  // drag control 
-  dControls = new DragControls(objects, camera, renderer.domElement); 
-  dControls.addEventListener('dragstart', function (event) { 
-    oControls.enabled = false; 
-  }); 
-  dControls.addEventListener('dragend', function (event) { 
-    oControls.enabled = true; 
-  }); 
-  dControls.addEventListener('drag', function (event) { 
-    raycaster.setFromCamera(touch, camera); 
-    raycaster.ray.intersectPlane(plane, intersects); 
-    event.object.position.set(intersects.x, intersects.y + event.object.scale.y / 2, intersects.z); 
-  });
-
+  document.body.appendChild( VRButton.createButton( renderer ) );
+  renderer.xr.enabled = true;
 }
 
 function onWindowResize() {
@@ -90,8 +48,7 @@ function onWindowResize() {
 }
 
 function animate() {
-  renderer.render(scene, camera);
-  requestAnimationFrame(animate);
+  renderer.setAnimationLoop( function () { renderer.render( scene, camera ); } );
 }
 
 init();
--- objects.js
+++ objects.js
@@ -1,21 +1,25 @@
 import * as THREE from 'three'
 
-function createFloor(scene) {
-    let pos = { x: 0, y: -1, z: 3 };
-    let scale = { x: 100, y: 2, z: 100 };
+function createFloor(scene, center, size) {
+    let pos = center;
+    pos.y -= 1;
+    let scale = size;
 
-    let blockPlane = new THREE.Mesh(new THREE.BoxBufferGeometry(), new THREE.MeshPhongMaterial({ color: 0xf9c834 }));
+    const texture = new THREE.TextureLoader().load('./assets/stageFloor.jpg');
+
+    let blockPlane = new THREE.Mesh(new THREE.BoxBufferGeometry(), new THREE.MeshPhongMaterial({ map:texture }));
     blockPlane.position.set(pos.x, pos.y, pos.z);
     blockPlane.scale.set(scale.x, scale.y, scale.z);
     blockPlane.castShadow = true;
     blockPlane.receiveShadow = true;
     blockPlane.userData.ground = true;
     scene.add(blockPlane);
+    return blockPlane;
 }
 
 function createBox(scene, objects) {
-    let scale = { x: 6, y: 6, z: 6 };
-    let pos = { x: 15, y: scale.y / 2, z: 15 };
+    let scale = { x: 6, y: 6, z: 1 };
+    let pos = { x: 15, y: scale.y / 2, z: 70 };
 
     let box = new THREE.Mesh(new THREE.BoxBufferGeometry(), new THREE.MeshPhongMaterial({ color: 0xDC143C }));
     box.position.set(pos.x, pos.y, pos.z);
@@ -23,9 +27,9 @@ function createBox(scene, objects) {
     box.castShadow = true;
     box.receiveShadow = true;
     box.userData.draggable = true
-    box.userData.name = 'BOX';
     scene.add(box);
     objects.push(box);
+    return box;
 }
 
 export { createBox, createFloor };
